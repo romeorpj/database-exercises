dbf7f179f353d2f29d726bcd93f465cb545c934e
Richard Jackson <rpjackson2@gmail.com>
2022-05-02 11:58:38 -0500

    mysql tables exercises

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+drop table if exists albums;
+
+show tables;
+-- Just wanted to test if table would work with a mixture of upper and lowercase values
+create table if not exists album
+(
+    id int unsigned not null auto_increment primary key ,
+    artist VARCHAR(255) NOT NULL;
+    name VARCHAR(255) NOT NULL;
+    release_date INT;
+    sales DOUBLE;
+    genre varchar(255)
+);
---
+++ tables_lec.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+    CREATE TABLE IF NOT EXISTS users
+    (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(100) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        middle_name VARCHAR(255)
+    );
+CREATE TABLE IF NOT EXISTS cats
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    breed VARCHAR(255) NOT NULL,
+    age INT UNSIGNED NOT NULL
+);
