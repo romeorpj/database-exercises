1e71e57b2230619ad6afebb291b42ad8b8ee793f
Richard Jackson <rpjackson2@gmail.com>
2022-05-02 14:37:26 -0500

    added music data to the database table, added release year as a string to practice updating to a number, then i just changed the record to 1982

---
+++ albums_seeder.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+# RENAME TABLE album TO albums;
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('Eagles','Hotel California',1976,31.5,'Soft Rock'),
+      ('Whitney Houston', 'The Bodyguard',1992,32.4,'R&B'),
+      ('Eagles','Their Greatest Hits (1971-1975)',1976,41.2,'Country Rock'),
+      ('Michael Jackson','Thriller',1982,50.2,'Pop');
+
+#
+# UPDATE codeup_test_db.albums
+# SET release_date = 1982
+# WHERE artist = 'Michael Jackson';
