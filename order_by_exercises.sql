e746e120408600a1edcb90f4f57f207da0fe8795
Richard Jackson <rpjackson2@gmail.com>
2022-05-04 09:55:29 -0500

    finished working on mysql order by ecxercsises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,19 @@
 USE employees;
 
+# SELECT * FROM employees
+# WHERE first_name IN('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, first_name;
+
+# SELECT * FROM employees
+# WHERE first_name IN('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, first_name DESC;
+
 SELECT * FROM employees
 WHERE first_name IN('Irena', 'Vidya', 'Maya')
-ORDER BY first_name, last_name;
+AND last_name LIKE '%e%'
+ORDER BY emp_no;
+
+SELECT * FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya')
+  AND last_name LIKE '%e%'
+ORDER BY emp_no DESC;
