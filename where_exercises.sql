515e0bfe370899ca5a3c3e3d585c1420e3efb3ce
Richard Jackson <rpjackson2@gmail.com>
2022-05-04 09:29:29 -0500

    finished MYSQL Where selections exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,30 @@
 USE employees;
 
-    SELECT * FROM employees
-WHERE first_name IN('Irena', 'Vidya', 'Maya');
+# SELECT * FROM employees
+# WHERE first_name IN('Irena', 'Vidya', 'Maya')
+# AND gender = 'M';
+
+# SELECT * FROM employees
+# WHERE
+#       first_name = 'Irena' OR
+#       first_name = 'Vidya' OR
+#       first_name = 'Maya'
+#         AND gender = 'M';
+
+# SELECT * FROM employees
+# WHERE last_name LIKE 'e%';
+#
+# SELECT * FROM employees
+# WHERE last_name LIKE '%q%';
+
+# SELECT * FROM employees
+# WHERE last_name LIKE 'e%'
+# OR last_name LIKE '%e';
+
+# SELECT * FROM employees
+# WHERE last_name LIKE 'e%'
+#    AND last_name LIKE '%e';
 
 SELECT * FROM employees
-WHERE last_name LIKE 'e%';
-
-SELECT * FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
