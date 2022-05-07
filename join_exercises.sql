921eec1097232c23bfd8061c822205f42b0d991d
Richard Jackson <rpjackson2@gmail.com>
2022-05-07 13:28:21 -0500

    Just need to fix aggregate function error in question 4

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,6 +7,7 @@ USE employees;
 # JOIN departments AS dpts
 # ON dm.dept_no = dpts.dept_no
 # WHERE dm.to_date = '9999-01-01'
+# ORDER BY dpts.dept_name
 # ;
 
 # SELECT  dpts.dept_name AS 'Department Name',CONCAT(emp.first_name, ' ', emp.last_name) AS 'Department Manager'
@@ -18,12 +19,29 @@ USE employees;
 # WHERE dm.to_date = '9999-01-01'AND emp.gender = 'f'
 # ;
 
-SELECT  tt.title AS 'title',  COUNT(tt.title) AS 'Total'
-FROM employees AS emp
-JOIN titles AS tt
-ON emp.emp_no = tt.emp_no
-JOIN dept_emp AS demp
-ON emp.emp_no = demp.emp_no
-WHERE tt.to_date = '9999-01-01' AND demp.dept_no = 'd009'
-GROUP BY tt.title
+# SELECT  tt.title AS 'title',  COUNT(tt.title) AS 'Total'
+# FROM employees AS emp
+# JOIN titles AS tt
+# ON emp.emp_no = tt.emp_no
+# JOIN dept_emp AS demp
+# ON emp.emp_no = demp.emp_no
+# WHERE tt.to_date = '9999-01-01' AND demp.dept_no = 'd009'
+# GROUP BY tt.title
+# ;
+
+SELECT  dpts.dept_name AS 'Department Name',CONCAT(emp.first_name, ' ', emp.last_name) AS 'Department Manager', sal
+    .salary AS Salary
+FROM employees AS Emp
+JOIN dept_manager AS dm
+ON Emp.emp_no = dm.emp_no
+JOIN salaries AS sal
+ON Emp.emp_no = sal.emp_no
+JOIN departments AS dpts
+ON dm.dept_no = dpts.dept_no
+WHERE dm.to_date = '9999-01-01'
+GROUP BY dpts.dept_name
+ORDER BY dpts.dept_name
+
+
+
 ;
